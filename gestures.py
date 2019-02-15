2e48325438d1b88d1f5b5ee8c4f6108940088b45
Heather Song <hsong89@gatech.edu>
2019-02-14 19:18:10 -0500

    Got shimon playing notes working. Midi device works as expected. Replaces socket programming with python osc. Changed gestures to work with pythonosc. Current problem: shimon receiveing gesture data in weird format

--- gestures.py
+++ gestures.py
@@ -4,20 +4,20 @@ Heather
 '''
 def headShakeGood():
     messages = []
-    messages.append("/head-commands HEADTILT 0.2 20.")
-    messages.append("/head-commands HEADTILT 0.8 20.")
-    messages.append("/head-commands HEADTILT 1.2 20.")
-    messages.append("/head-commands HEADTILT 0.8 20.")
-    messages.append("/head-commands HEADTILT 1.2 20.")
+    messages.append(["HEADTILT", 0.2, 20])
+    messages.append(["HEADTILT", 0.8, 20])
+    messages.append(["HEADTILT", 1.2, 20])
+    messages.append(["HEADTILT", 0.8, 20])
+    messages.append(["HEADTILT", 1.2, 20])
     return messages
 
 def headShakeBad():
     messages = []
-    messages.append("/head-commands HEADTILT 1. 20.")
-    messages.append("/head-commands NECKPAN1 -1. 10.")
-    messages.append("/head-commands NECKPAN1 -.5 10.")
-    messages.append("/head-commands NECKPAN1 -1. 10.")
-    messages.append("/head-commands NECKPAN1 -.5 10.")
-    messages.append("/head-commands NECKPAN1 -1. 10.")
-    messages.append("/head-commands NECKPAN1 -.5 10.")
+    messages.append([" HEADTILT", 1, 20])
+    messages.append([" NECKPAN1", -1, 10])
+    messages.append([" NECKPAN1", -.5, 10])
+    messages.append([" NECKPAN1", -1, 10])
+    messages.append([" NECKPAN1", -.5, 10])
+    messages.append([" NECKPAN1", -1, 10])
+    messages.append([" NECKPAN1", -.5, 10])
     return messages
--- shimonGame.py
+++ shimonGame.py
@@ -1,8 +1,8 @@
 import random
 import rtmidi_python as rtmidi
-import socket
+from pythonosc import udp_client
 import gestures
-
+import time
 #remake gestures in python
 #test connection with shimon
 
@@ -13,19 +13,18 @@ MUSIC_PORT = 51973
 #pedal inputs are : 48, 50, 52, 53
 #mididata 60 60
 #velocity between 0 - 127
-print("UDP target IP:", UDP_IP)
-print("UDP target port:", UDP_PORT)
-print("message:", MESSAGE)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 
+clientmusic = udp_client.SimpleUDPClient(SHIMON_IP, MUSIC_PORT)
+clientGest = udp_client.SimpleUDPClient(SHIMON_IP, GESTURE_PORT)
 def start():
-    print("initial shimon gesture")
+    print("start")
     tutorial()
 
 def tutorial():
     correct = False
     while not correct:
+        time.sleep(4)
         playNotes([60,62,64,65])
         count = 0
         userPlayed = []
@@ -35,7 +34,16 @@ def tutorial():
                 count += 1
                 if count % 2 == 1:
                     userPlayed.append(message[1])
+                    if message[1] == 48:
+                        playNotes([60])
+                    if message[1] == 50:
+                        playNotes([62])
+                    if message[1] == 42:
+                        playNotes([64])
+                    if message[1] == 53:
+                        playNotes([65])
                     print(message[1])
+                    time.sleep(1)
         if userPlayed == [48,50,52,53]: # 1,2,3,4 is just placeholder I don't know what message actually looks like
             correctGesture()
             correct = True
@@ -72,15 +80,19 @@ Takes in array of numbers(notes)
 '''
 def playNotes(arr):
     for note in arr:
-        sock.sendto(bytes("/mididata" + str(note) + "110", "utf-8"), (SHIMON_IP , MUSIC_PORT))
+        clientmusic.send_message("/mididata", [note,  110])
+        #sock.sendto(bytes("/mididata " + str(note) + " 110", "utf-8"), (SHIMON_IP , MUSIC_PORT))
+        time.sleep(1)
     print("Shimon played notes:", arr)
 
 '''
 Method that makes shimon do the gesture indicating correct notes by the player
 '''
-def correctGesture(sock):
-    message = gestures.headShakegood()
-    sock.sendto(bytes(arr, "utf-8"), (SHIMON_IP , GESTURE_PORT))
+def correctGesture():
+    message = gestures.headShakeGood()
+    for part in message:
+        filter1 = "/head-commands " + part[0]
+        clientmusic.send_message(filter1, part[1:])
     print("shimon does good gesture")
 
 
@@ -88,10 +100,15 @@ def correctGesture(sock):
 Method that makes shimon do the gesture indicating incorrect notes by the player
 '''
 def wrongGesture():
+    message = gestures.headShakeBad()
+    for part in message:
+        filter1 = "/head-commands " + part[0]
+        clientmusic.send_message(filter1, part[1:])
+        time.sleep(1)
     print("shimon does bad gesture")
 
 
-print("start")
+
 midi_in = rtmidi.MidiIn(b"input")
 midi_in.open_port(0)
 start()
